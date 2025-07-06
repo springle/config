a8f573e43d74c019347011177a803a477ee8c425
Sam Pringle <pringle@hey.com>
2025-07-06 18:33:56 -0400

    Install nvim-treesitter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,8 @@
 -- INIT.LUA
 --
+-- GLOBAL MARKS (conventions)
+-- `I => init.lua
+--
 -- KEY BINDINGS (built-in)
 -- q: => open command history
 -- :hi => open highlight groups for current buffer
@@ -17,6 +20,7 @@
 -- PLUGINS
 -- mbbill/undotree
 -- neanias/everforest-nvim
+-- nvim-treesitter/nvim-treesitter
 
 -- colorscheme
 vim.cmd.colorscheme('mcm')
@@ -26,3 +30,55 @@ vim.g.mapleader = " "
 
 -- configure undo tree
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- configure treesitter
+require'nvim-treesitter.configs'.setup {
+  -- Directory to install parsers and queries to
+  parser_install_dir = vim.fn.stdpath('data') .. '/site',
+
+  -- Ensure these parsers are installed
+  ensure_installed = {
+    'c',
+    'css',
+    'csv',
+    'diff',
+    'dockerfile',
+    'git_config',
+    'git_rebase',
+    'gitattributes',
+    'gitcommit',
+    'gitignore',
+    'graphql',
+    'helm',
+    'html',
+    'http',
+    'ini',
+    'java',
+    'javascript',
+    'jq',
+    'json',
+    'kdl',
+    'make',
+    'markdown',
+    'markdown_inline',
+    'mermaid',
+    'nginx',
+    'python',
+    'regex',
+    'requirements',
+    'rust',
+    'sql',
+    'ssh_config',
+    'terraform',
+    'tmux',
+    'toml',
+    'typescript',
+    'xml',
+    'yaml'
+  },
+
+  -- Enable syntax highlighting
+  highlight = {
+    enable = true,
+  },
+}
